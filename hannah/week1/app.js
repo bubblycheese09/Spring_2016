a234c52fbef06f6370ed055404dac3c266a0d8fe
Hannah Spierer <joelspierer@Hannahs-MacBook-Pro.local>
2016-02-17 18:22:34 -0500

    first push

---
+++ hannah/week1/app.js
@@ -0,0 +1,29 @@
+//alert("yo")
+// var userName = prompt("What's your name?");
+// alert("hi " + userName);
+// var coder = confirm("do you code?");
+
+// if(coder==true){
+// 	document.write(userName + " is a coder!"); //click ok to get this message
+// }else{
+// 	document.write(userName + " is not a coder!"); //click cancel to get this message
+// };
+
+// var age=parseInt(prompt("What's your age?")); //parseInt ensures what's entered is converted to an integer
+
+// if (age > 30){
+// 	var newAge=age+10;
+// 	alert("the camera adds 10 years");
+// }else{
+// 	alert("cool");
+// };
+
+
+var name=prompt("What's your name");
+var age=prompt("how old are you");
+if (age >= 18){
+	var choose=prompt("who will u vote 4? choose: kanye, voldemort, or squidward");
+}else{
+	alert("try again next year :(");
+};
+
---
+++ hannah/week1/homework/myCalc-master/app.js
@@ -0,0 +1,50 @@
+console.log('yar! js working.');
+
+// Your code here!
+
+// //declare variables
+var answer;
+
+var operation;
+var number1;
+var number2;
+
+operation = prompt("Choose a mathematical operator: addition, subtraction, multiplication, division, modulo, or exponent.");
+number1 = parseInt(prompt("Type in a number")); //parseInt(): turn string input into an integer
+number2 = parseInt(prompt("Type in another number."));
+
+//control flow to determine how to handle the inputted operation and numbers
+if(operation == "addition"){
+	answer = number1 + number2; 
+	alert(number1 + " + " + number2 + " = " + answer);
+}else if(operation == "subtraction"){
+	if(number1 > number2){
+		answer = (number1) - (number2);
+		alert(number1 + " - " + number2 + " = " + answer);
+	}else{
+		answer = (number2) - (number1);
+		alert(number1 + " - " + number2 + " = -" + answer);
+	}
+}else if(operation == "multiplication"){
+	answer = number1 * number2;
+	alert(number1 + " * " + number2 + " = " + answer);
+}else if(operation == "division"){
+	if(number1 > number2){
+		answer = number1 / number2;
+		alert(number1 + " / " + number2 + " = " + answer);
+	}else{
+		parseFloat(answer = number1 / number2); //parseFloat() turns input into float
+		alert(number1 + " / " + number2 + " = " + answer);
+	}
+}else if(operation=="modulo"){
+	answer = number1 % number2;
+	alert("The remainder of " + number1 + " and " + number2 + " is " + answer);
+}else if(operation=="exponent"){
+	answer = Math.pow(number1, number2); //Math.pow() calculates exponents
+	alert(number1 + " ^ " + number2 + " = " + answer);
+}else{
+	alert("Enter an operation and try again!")
+}
+
+
+
